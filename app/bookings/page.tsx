4cf2541ed980a9059d77bc0912a2784b3a228309
Sebastião Rodrigol <rodrigoexer1@gmail.com>
2025-04-28 17:58:54 -0300

    updated-bookings-page

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,59 +1,29 @@
-import { db } from "../_lib/prisma";
-import { authOptions } from "@/app/_lib/auth"
 import Header from "../_components/header";
 import { notFound } from "next/navigation";
 import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/_lib/auth";
 import BookingItem from "../_components/booking-item";
+import { getConcludedBookings } from "../_data/get-concluded-bookings";
+import { getConfirmedBookings } from "../_data/get-confirmed-bookings";
 
 const Bookings = async () => {
     const session = await getServerSession(authOptions)
     if(!session?.user){
+        // TODO: mostrar pop-up login
         return notFound()
     }
-    const confirmedBookings = await db.booking.findMany({
-        where: {
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            userId: (session?.user as any).id,
-            date: {
-                gte: new Date()
-            }
-        },
-        include: {
-            service: {
-                include: {
-                    barbershop: true,
-                },
-            },
-        },
-        orderBy: {
-            date: "asc",
-        }
-    })
-    const concludedBookings = await db.booking.findMany({
-        where: {
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            userId: (session?.user as any).id,
-            date: {
-                lt: new Date()
-            }
-        },
-        include: {
-            service: {
-                include: {
-                    barbershop: true,
-                },
-            },
-        },
-        orderBy: {
-            date: "asc",
-        }
-    })
+
+    const confirmedBookings = await getConfirmedBookings()
+    const concludedBookings = await getConcludedBookings()
 
     return (
         <>
             <Header />
             <div className="space-y-3 p-5">
                 <h1 className="text-xl font-bold">Agendamentos</h1>
+                {confirmedBookings.length === 0 && concludedBookings.length === 0 && (
+                    <p className="text-gray-400">Você não tem agendamentos.</p>
+                )}
 
                 {confirmedBookings.length > 0 && (
                     <>
@@ -61,7 +31,10 @@ const Bookings = async () => {
                             Confirmados
                         </h2>
                         {confirmedBookings.map((booking) =>
-                            <BookingItem key={booking.id} booking={booking} />
+                            <BookingItem
+                                key={booking.id}
+                                booking={JSON.parse(JSON.stringify(booking))}
+                            />
                         )}
                     </>
                 )}
@@ -72,7 +45,10 @@ const Bookings = async () => {
                             Finalizados
                         </h2>
                         {concludedBookings.map((booking) =>
-                            <BookingItem key={booking.id} booking={booking} />
+                            <BookingItem
+                                key={booking.id}
+                                booking={JSON.parse(JSON.stringify(booking))}
+                            />
                         )}
                     </>
                 )}
